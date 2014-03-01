2db42facc15e5f0a03a5d7968084ec6487a7ccee
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-03-01 21:10:57 +0100

    First steps with the filter() function

--- src/treeview/treeview.js
+++ src/treeview/treeview.js
@@ -2,9 +2,12 @@
 
 define('treeview', [], function() {
 
+  "use strict";
+  
   function Node(level) {
     this.level = level;
     this.children = [];
+    this.leaf = ko.observable(false);
   }
   
   // Create a Node for each level
@@ -13,41 +16,56 @@ define('treeview', [], function() {
   
     var options = options || {};
   
-    return objectToNode(obj, []);
+    return objectToNode(obj, null, []);
   
-    function objectToNode(obj, parents) {
+    function objectToNode(obj, key, parents) {
       //console.log('objectToNode()', obj, parents);
       
       var node = new Node(parents.length);
       
-      if (_.isArray(obj)) {
-        var items = _.filter(obj, function(item) { return _.isObject(item); } );
-        _.each(items, function(item, index) {
-          //console.log('array child node #'+index+':', item.toString(), parents.length);
-          var child = makeChildNode(item);
-          child.name = '#' + index;
-          // TODO: recurse depending on item type
-          node.children.push( child );
-        });
-      }
-      else if (_.isObject(obj)) {
-        _.each(obj, function(item, key) {
-          if (_.isObject(item)) {
-            var child = makeChildNode(item);
-            child.name = key;
-            node.children.push( child );
+      var usage = options.filter ? options.filter(node, obj, key, parents) : node;
+      
+      if (usage !== false) {
+        
+        if (typeof usage !== 'undefined') { 
+          // TODO: special usage options
+        }
+        
+        if (!node.leaf()) {
+          if (_.isArray(obj)) {
+            var items = _.filter(obj, function(item) { return _.isObject(item); } );
+            _.each(items, function(item, index) {
+              //console.log('array child node #'+index+':', item.toString(), parents.length);
+              var child = makeChildNode(item, index);
+              if (child) {
+                child.name = '#' + index;
+                // TODO: recurse depending on item type
+                node.children.push( child );
+              }
+            });
           }
-        });
-      }
-      else {
-        throw new Error('TreeView.fromJSON(): unsupported object type:', obj);
+          else if (_.isObject(obj)) {
+            _.each(obj, function(item, key) {
+              if (_.isObject(item)) {
+                var child = makeChildNode(item, key);
+                if (child) {
+                  child.name = key;
+                  node.children.push( child );
+                }
+              }
+            });
+          }
+          else {
+            throw new Error('TreeView.fromJSON(): unsupported object type:', obj);
+          }
+        }
       }
       
       return node;
       
       //---
       
-      function makeChildNode(item) { return objectToNode(item, parents.concat([obj])); }
+      function makeChildNode(item, key) { return objectToNode(item, key, parents.concat([obj])); }
     }
   }
   
