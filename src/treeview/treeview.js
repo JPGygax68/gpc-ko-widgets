4837a8d32f3ea401a22414bb7c5f9334d4ec9cc6
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-03-04 22:02:36 +0100

    Using observableArrays now; improved fromJSON() logic

--- src/treeview/treeview.js
+++ src/treeview/treeview.js
@@ -6,7 +6,7 @@ define('treeview', [], function() {
   
   function Node(level, label) {
     this.level = level;
-    this.children = [];
+    this.children = ko.observableArray();
     this.label = ko.observable(label);
     this.open = ko.observable(true);
     this.leaf = ko.observable(false);
@@ -33,18 +33,17 @@ define('treeview', [], function() {
     var options = options || {};
   
     return {
-      rootNode: objectToNode(obj, null, '(ROOT)', []),
+      rootNode: itemToNode(obj, null, '(ROOT)', []),
       showRoot: ko.observable(false)
     };
   
-    function objectToNode(obj, key, label, parents) {
-      //console.log('objectToNode()', obj, parents);
+    function itemToNode(item, key, label, parents) {
+      //console.log('itemToNode()', item, parents);
       
       var node = new Node(parents.length, label);
       
-      var usage = options.filter ? options.filter(node, obj, key, parents) : node;
-      
-      if (usage === false) return null; // filter discarded this item
+      node.leaf( !_.isObject(item) );
+      var usage = options.filter ? options.filter(node, item, key, parents) : node;
       
       if (usage !== false) {
         
@@ -52,39 +51,46 @@ define('treeview', [], function() {
           // TODO: special usage options
         }
         
-        if (!node.leaf()) {
-          if (_.isArray(obj)) {
-            var items = _.filter(obj, function(item) { return _.isObject(item); } );
-            _.each(items, function(item, index) {
+        if (_.isArray(item)) {
+          var items = _.filter(item, function(item) { return _.isObject(item); } );
+          if (!node.leaf()) {
+            _.each(items, function(subitem, index) {
               //console.log('array child node #'+index+':', item.toString(), parents.length);
-              var child = makeChildNode(item, index, '#' + index);
+              var child = makeChildNode(subitem, index, '#' + index);
               if (child) {
                 // TODO: recurse depending on item type
                 node.children.push( child );
               }
             });
           }
-          else if (_.isObject(obj)) {
-            _.each(obj, function(item, key) {
-              if (_.isObject(item)) {
-                var child = makeChildNode(item, key, key);
-                if (child) node.children.push( child );
-              }
+        }
+        else if (_.isObject(item)) {
+          //console.log('item is object');
+          if (!node.leaf()) {
+            _.each(item, function(subitem, subkey) {
+              //console.log('  ', subkey, ':', subitem);
+              var child = makeChildNode(subitem, subkey, subkey);
+              if (child) node.children.push( child );
             });
           }
-          else {
-            throw new Error('TreeView.fromJSON(): unsupported object type:', obj);
-          }
         }
+        else {
+          //throw new Error('TreeView.fromJSON(): unsupported item type:', item);
+          node.value = ko.observable(item);
+          node.leaf(true);
+        }
+        
+        return node;
       }
-      else console.log('skipping content');
-      
-      return node;
+      else {
+        console.log('skipping content');
+        return null;
+      }
       
       //---
       
       function makeChildNode(subitem, subkey, label) { 
-        return objectToNode(subitem, subkey, label, parents.concat([{key: key, obj: obj}])); }
+        return itemToNode(subitem, subkey, label, parents.concat([{key: key, obj: obj}])); }
     }
   }
   
