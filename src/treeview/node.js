c9a5e0f851a1bb92c0b39c50d7751c14a3edb936
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-03-08 14:57:32 +0100

    Started implementing keyboard navigation

--- src/treeview/node.js
+++ src/treeview/node.js
@@ -2,6 +2,8 @@
 
 define(['./node', './defs'], function(Node, Defs) {
 
+  "use strict";
+  
   function Node(treeview, parent, level, label) {
     this.treeview = treeview;
     // TODO: should parent and level be observables too ? (in case nodes are being moved around?)
@@ -39,8 +41,29 @@ define(['./node', './defs'], function(Node, Defs) {
     this.labelWidthCss = ko.computed( function() { return this.hasValue() ? this.labelWidth()+'px' : ''; }, this );
     this.labelColspan = ko.computed( function() {
       return (this.leaf() ? 2 : 1) + (this.hasValue() ? 1 : 0);
-    }, this);    
+    }, this);
+    this.hasFocus = ko.observable(false);
   }
+
+  // INTERNAL METHODS -------------------------
+  
+  Node.prototype._getSiblingOf = function(child, offset) {
+    var i = _.indexOf(this.children(), child);
+    if (i < 0) throw new Error('_getSiblingOf(): starting node not found among children of parent');
+    if ((i + offset) >= 0 && (i + offset) < this.children().length) return this.children()[i + offset]; else return null;
+  };
+  
+  Node.prototype._goToSibling = function(offset) {
+    if (!!this.parent) {
+      var sibling = this.parent._getSiblingOf(this, offset);
+      if (!!sibling) { sibling.hasFocus(true); return true; }
+    }
+  };
+  
+  //Node.prototype.getSuccessorOf   = function(child) { return this._getSiblingOf(child,  1); }  
+  //Node.prototype.getPredecessorOf = function(child) { return this._getSiblingOf(child, -1); }
+  
+  // EVENT HANDLERS -----------------------------
   
   Node.prototype.onClick = function(self, event) {
     console.log('Node.onClick():', self, event);
@@ -51,11 +74,30 @@ define(['./node', './defs'], function(Node, Defs) {
   
   Node.prototype.onMouseOverLabel = function(self, event) {
     console.log('Node.onMouseOverLabel():', self, event);
-  }
+  };
+  
   Node.prototype.onMouseLeaveLabel = function(self, event) {
     console.log('Node.onMouseLeaveLabel():', self, event);
-  }
+  };
+  
+  Node.prototype.onKeyDown = function(self, event) {
+    console.log('Node.onKeyDown():', self.label(), event, 'key:', event.key);
 
+    // TODO: use a "shortcut" registry to link keyboard input to actions
+    // TODO: formalize "actions"
+    // TODO: decouple return value of action and return value of event handler (which determines further handling)
+    if      (!!event.key ? (event.key === 'Down') : (event.which === 40)) if (this.goToSuccessor  ()) return false; }
+    else if (!!event.key ? (event.key === 'Up'  ) : (event.which === 38)) if (this.goToPredecessor()) return false; }
+    
+    return true;
+  };
+
+  // ACTIONS ---------------------
+  
+  Node.prototype.goToSuccessor   = function() { this._goToSibling( 1); }
+  
+  Node.prototype.goToPredecessor = function() { this._goToSibling(-1); }
+  
   // EXPORT --------------
   
   return Node;
