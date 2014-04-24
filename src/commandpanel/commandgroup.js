0d5c22a99b1d941e9b77ae3045fa8c9fe7f2c89e
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-04-24 22:21:18 +0200

    A bit of restructuring

---
+++ src/commandpanel/commandgroup.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var ko = require('knockout');
+
+var Command = require('./command');
+
+function CommandGroup() {
+
+  this.commands = ko.observableArray();
+
+  this.top = ko.observable(0);  
+  this.height = ko.observable(0);
+
+  this.target = ko.observable(null);   
+
+  this.commands.subscribe( function(changes) {
+    changes.forEach( function(change) {
+      //change.index, change.status, change.value
+      // So we don't have to pass owner when constructing objects
+      if (change.status === 'added') change.value._owner = this;
+    }, this);
+  }, this, 'arrayChange');  
+}
+
+CommandGroup.prototype._delegated_keydown = function(view_model, event) {
+  //console.log('_delegated_keydown()');
+  
+  for (var i = 0; i < this.commands().length; i ++) {
+    var cmd = this.commands()[i];
+    if (ko.unwrap(cmd.enabled)) {
+      if (typeof cmd.shortcut !== 'undefined' && Keyboard.is(event, cmd.shortcut)) { 
+        cmd.trigger();
+        event.preventDefault();
+        event.stopPropagation();
+        break;
+      }
+    }
+  }
+};
+
+CommandGroup.prototype.setTargetZone = function(x, y, w, h) {
+  //console.log('CommandGroup::setTargetZone()', x, y, w, h);
+  
+  this.top( y + h / 2 );
+  this.height( h );
+};
+
+CommandGroup.prototype.alignWithElement = function(elt) {
+  var rect = elt.getBoundingClientRect();
+  this.setTargetZone(rect.left, rect.top, rect.width, rect.height);
+};
+
+CommandGroup.prototype.delegate = function(event_name, view_model, event_data) {
+  //console.log('CommandGroup::delegate()', event_name, view_model, event_data);
+  
+  return this['_delegated_'+event_name](view_model, event_data);
+};
+
+// EXPORTS -----------------------
+
+module.exports = CommandGroup;
--- src/commandpanel/commandpanel.js
+++ src/commandpanel/commandpanel.js
@@ -4,6 +4,8 @@ var ko = require('knockout');
 
 var Command = require('./command');
 
+var CommandGroup = require('./commandgroup');
+
 var templates = require('../../generated/commandpanel/templates');
   
 // Store our templates in the string template engine (guarded)
@@ -24,61 +26,9 @@ function CommandPanel() {
   }, this, 'arrayChange');  
 }
 
-function Group() {
-
-  this.commands = ko.observableArray();
-
-  this.top = ko.observable(0);  
-  this.height = ko.observable(0);
-
-  this.target = ko.observable(null);   
-
-  this.commands.subscribe( function(changes) {
-    changes.forEach( function(change) {
-      //change.index, change.status, change.value
-      // So we don't have to pass owner when constructing objects
-      if (change.status === 'added') change.value._owner = this;
-    }, this);
-  }, this, 'arrayChange');  
-}
-
-Group.prototype._delegated_keydown = function(view_model, event) {
-  //console.log('_delegated_keydown()');
-  
-  for (var i = 0; i < this.commands().length; i ++) {
-    var cmd = this.commands()[i];
-    if (ko.unwrap(cmd.enabled)) {
-      if (typeof cmd.shortcut !== 'undefined' && Keyboard.is(event, cmd.shortcut)) { 
-        cmd.trigger();
-        event.preventDefault();
-        event.stopPropagation();
-        break;
-      }
-    }
-  }
-};
-
-Group.prototype.setTargetZone = function(x, y, w, h) {
-  //console.log('Group::setTargetZone()', x, y, w, h);
-  
-  this.top( y + h / 2 );
-  this.height( h );
-};
-
-Group.prototype.alignWithElement = function(elt) {
-  var rect = elt.getBoundingClientRect();
-  this.setTargetZone(rect.left, rect.top, rect.width, rect.height);
-};
-
-Group.prototype.delegate = function(event_name, view_model, event_data) {
-  //console.log('Group::delegate()', event_name, view_model, event_data);
-  
-  return this['_delegated_'+event_name](view_model, event_data);
-};
-
 // We make stuff available by attaching it to the constructor of the CommandPanel class.
 CommandPanel.Command = Command;
-CommandPanel.Group   = Group;
+CommandPanel.Group   = CommandGroup;
 
 // EXPORTS -----------------------
 
