99fd5763c77eef0b66897f400c912317f62dff87
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-05-03 00:36:50 +0200

    Automatic commit (programmer forgot)

--- cleanup.bat
+++ cleanup.bat
@@ -2,6 +2,7 @@
 
 git add * --all
 if %errorlevel% equ 0 git commit -m "Automatic commit (programmer forgot)"
+if %errorlevel% equ 0 git remote update
 if %errorlevel% equ 0 git pull origin --all
 if %errorlevel% equ 0 git push origin --all
 
