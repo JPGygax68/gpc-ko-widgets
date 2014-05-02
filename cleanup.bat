f6a5708d206675cf3d44a8b909ae23231d0a3d10
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-05-02 22:23:06 +0200

    logon.bat, cleanup.bat

---
+++ cleanup.bat
@@ -0,0 +1,8 @@
+@echo off
+
+git add * --all
+if %errorlevel% equ 0 git commit -m "Automatic commit (programmer forgot)"
+if %errorlevel% equ 0 git pull origin
+if %errorlevel% equ 0 git push origin --all
+
+:end
